24f4fc22f0de17dd454d9f227dfeeb2821de5442
Anna Litvinova <litvinova4815@gmail.com>
2022-05-24 23:36:53 +0300

    Added new files

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double m = 51;
+        double h = 171;
+        double BMI = service.calculate(m, h);
+        System.out.println("Индекс массы тела " + BMI);
+
+    }
 }
